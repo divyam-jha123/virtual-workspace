55346c3c34ee39967e9a670d281dba3d05912366
divyamjha <divyamjha.70055594@gmail.com>
2026-08-23 00:18:39 +0530

    reception area design

--- content/tilesets/office-props.tsx
+++ content/tilesets/office-props.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="office-props" tilewidth="64" tileheight="61" tilecount="81" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="office-props" tilewidth="96" tileheight="64" tilecount="126" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../tiles/props/monitor_off.png" width="16" height="19"/>
@@ -243,5 +243,140 @@
  </tile>
  <tile id="80">
   <image source="../tiles/props/water-filter.png" width="32" height="48"/>
+ </tile>
+ <tile id="81">
+  <image source="../tiles/props/lobby-reception-desk-full.png" width="64" height="27"/>
+ </tile>
+ <tile id="82">
+  <image source="../tiles/props/lobby-reception-counter.png" width="64" height="35"/>
+ </tile>
+ <tile id="83">
+  <image source="../tiles/props/lobby-office-chair.png" width="50" height="64"/>
+ </tile>
+ <tile id="84">
+  <image source="../tiles/props/lobby-monitor.png" width="64" height="64"/>
+ </tile>
+ <tile id="85">
+  <image source="../tiles/props/lobby-sofa-3-seat.png" width="64" height="24"/>
+ </tile>
+ <tile id="86">
+  <image source="../tiles/props/lobby-armchair.png" width="58" height="64"/>
+ </tile>
+ <tile id="87">
+  <image source="../tiles/props/lobby-coffee-table.png" width="64" height="30"/>
+ </tile>
+ <tile id="88">
+  <image source="../tiles/props/lobby-area-rug.png" width="64" height="35"/>
+ </tile>
+ <tile id="89">
+  <image source="../tiles/props/lobby-notebook.png" width="40" height="48"/>
+ </tile>
+ <tile id="90">
+  <image source="../tiles/props/lobby-plant-round.png" width="53" height="64"/>
+ </tile>
+ <tile id="91">
+  <image source="../tiles/props/lobby-plant-tall.png" width="41" height="64"/>
+ </tile>
+ <tile id="92">
+  <image source="../tiles/props/lobby-water-cooler.png" width="37" height="64"/>
+ </tile>
+ <tile id="93">
+  <image source="../tiles/props/lobby-plant-square.png" width="50" height="64"/>
+ </tile>
+ <tile id="94">
+  <image source="../tiles/props/layout-floor-cross-bottom.png" width="16" height="16"/>
+ </tile>
+ <tile id="95">
+  <image source="../tiles/props/layout-floor-cross-top.png" width="16" height="16"/>
+ </tile>
+ <tile id="96">
+  <image source="../tiles/props/layout-floor-l-bottom.png" width="16" height="16"/>
+ </tile>
+ <tile id="97">
+  <image source="../tiles/props/layout-floor-l-top.png" width="16" height="16"/>
+ </tile>
+ <tile id="98">
+  <image source="../tiles/props/layout-floor-pill-bottom.png" width="16" height="16"/>
+ </tile>
+ <tile id="99">
+  <image source="../tiles/props/layout-floor-pill-top.png" width="16" height="16"/>
+ </tile>
+ <tile id="100">
+  <image source="../tiles/props/layout-floor-rect-bottom.png" width="16" height="16"/>
+ </tile>
+ <tile id="101">
+  <image source="../tiles/props/layout-floor-rect-top.png" width="16" height="16"/>
+ </tile>
+ <tile id="102">
+  <image source="../tiles/props/layout-floor-square-bottom.png" width="16" height="16"/>
+ </tile>
+ <tile id="103">
+  <image source="../tiles/props/layout-floor-square-top.png" width="16" height="16"/>
+ </tile>
+ <tile id="104">
+  <image source="../tiles/props/layout-floor-t-bottom.png" width="16" height="16"/>
+ </tile>
+ <tile id="105">
+  <image source="../tiles/props/layout-floor-t-top.png" width="16" height="16"/>
+ </tile>
+ <tile id="106">
+  <image source="../tiles/props/layout-room-frame-oct-bottom.png" width="64" height="39"/>
+ </tile>
+ <tile id="107">
+  <image source="../tiles/props/layout-room-frame-oct-top.png" width="64" height="62"/>
+ </tile>
+ <tile id="108">
+  <image source="../tiles/props/layout-rug-big-outline.png" width="64" height="53"/>
+ </tile>
+ <tile id="109">
+  <image source="../tiles/props/layout-rug-cross-fill.png" width="64" height="61"/>
+ </tile>
+ <tile id="110">
+  <image source="../tiles/props/layout-rug-cross-outline.png" width="64" height="61"/>
+ </tile>
+ <tile id="111">
+  <image source="../tiles/props/layout-rug-l-fill.png" width="64" height="54"/>
+ </tile>
+ <tile id="112">
+  <image source="../tiles/props/layout-rug-l-outline.png" width="64" height="54"/>
+ </tile>
+ <tile id="113">
+  <image source="../tiles/props/layout-rug-pill-fill.png" width="41" height="64"/>
+ </tile>
+ <tile id="114">
+  <image source="../tiles/props/layout-rug-pill-outline.png" width="40" height="64"/>
+ </tile>
+ <tile id="115">
+  <image source="../tiles/props/layout-rug-rect-fill.png" width="64" height="57"/>
+ </tile>
+ <tile id="116">
+  <image source="../tiles/props/layout-rug-rect-outline.png" width="64" height="27"/>
+ </tile>
+ <tile id="117">
+  <image source="../tiles/props/layout-rug-square-fill.png" width="60" height="64"/>
+ </tile>
+ <tile id="118">
+  <image source="../tiles/props/layout-rug-square-outline.png" width="64" height="64"/>
+ </tile>
+ <tile id="119">
+  <image source="../tiles/props/layout-wall-cross-top.png" width="57" height="64"/>
+ </tile>
+ <tile id="120">
+  <image source="../tiles/props/layout-wall-l-top.png" width="64" height="53"/>
+ </tile>
+ <tile id="121">
+  <image source="../tiles/props/layout-wall-pill-top.png" width="41" height="64"/>
+ </tile>
+ <tile id="122">
+  <image source="../tiles/props/layout-wall-rect-top.png" width="64" height="39"/>
+ </tile>
+ <tile id="123">
+  <image source="../tiles/props/layout-wall-square-top.png" width="64" height="64"/>
+ </tile>
+ <tile id="124">
+  <image source="../tiles/props/layout-wall-t-top.png" width="60" height="64"/>
+ </tile>
+ <tile id="125">
+  <image source="../tiles/props/reception-signboard.png" width="96" height="32"/>
  </tile>
 </tileset>
